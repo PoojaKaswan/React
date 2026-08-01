3b118608d19a08662d8916bb6462bea7c26f929d
Pooja Kaswan <gettoknowpooja@outlook.com>
2026-08-01 13:32:30 +0530

    Adding props

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
       <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind test</h1>
+      <Card username="ChaiAurCode" btnText="click me" />
       <Card />
     </> 
   )
