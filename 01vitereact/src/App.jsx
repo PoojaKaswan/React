98255fbcd8ab15bf8ff30923f7a36a473f0fb4b1
Pooja Kaswan <gettoknowpooja@outlook.com>
2026-08-15 19:34:19 +0530

    corrected a typo

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 import Chai from './Chai'
 
 function App() {
-  const usernamr = "chai aur code"
+  const username = "chai aur code"
   return (
     <>
-    <Chai/>
-    <h1>Chai aur react with vite {usermame}</h1>
+      <Chai/>
+      <h1>Chai aur react with vite {usermame}</h1>
     </>
   )
 }
